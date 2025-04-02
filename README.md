0fed769d043e9755111b4f69dd73d37aa1a01d99
Natalia K <natalia.kielbasa2002@gmail.com>
2025-04-02 21:52:22 +0200

    zad2 - added product requests

--- README.md
+++ README.md
@@ -15,8 +15,8 @@ Dodatkowo:
 
 ## Zadanie 2 - Scala - Kontroler do produktów
 Należy stworzyć aplikację na frameworku Play w Scali 3. 
-- [ ] 3.0 Należy stworzyć kontroler do Produktów
-- [ ] 3.5 Do kontrolera należy stworzyć endpointy zgodnie z CRUD - dane pobierane z listy
+- [x] 3.0 Należy stworzyć kontroler do Produktów
+- [x] 3.5 Do kontrolera należy stworzyć endpointy zgodnie z CRUD - dane pobierane z listy
 - [ ] 4.0 Należy stworzyć kontrolery do Kategorii oraz Koszyka + endpointy zgodnie z CRUD
 - [ ] 4.5 Należy aplikację uruchomić na dockerze (stworzyć obraz) oraz dodać skrypt uruchamiający aplikację via ngrok
 - [ ] 5.0 Należy dodać konfigurację CORS dla dwóch hostów dla metod CRUD
--- zad_2/product-controler/app/controllers/ProductController.scala
+++ zad_2/product-controler/app/controllers/ProductController.scala
@@ -3,9 +3,70 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import play.api.libs.json._
+import scala.collection.mutable
 import models.Product
 
 @Singleton
 class ProductController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
+    private val productList = new mutable.ListBuffer[Product]()
+    productList += Product(1, "apple")
+    productList += Product(2, "bear")
+    productList += Product(3, "chainsaw")
+    productList += Product(4, "double cheesburger")
+    productList += Product(5, "epipen")
+
+    implicit val productFormat: OFormat[Product] = Json.format[Product]
+
+    def getAllProducts: Action[AnyContent] = Action {
+        if (productList.isEmpty) {
+            NoContent
+        }
+        else {
+            Ok(Json.toJson(productList))
+        }
+    }
+
+    def getProduct(id: Int): Action[AnyContent] = Action {
+        productList.find(_.id == id) match {
+            case Some(product) => Ok(Json.toJson(product))
+            case None => NotFound
+        }
+    }
+
+    def addProduct: Action[AnyContent] = Action { implicit request =>
+        request.body.asJson.flatMap(Json.fromJson[Product](_).asOpt) match {
+            case Some(product) =>
+                val nextId = productList.map(_.id).max + 1
+                val newProduct = product.copy(id = nextId)
+                productList += newProduct
+                Created(Json.toJson(newProduct))
+            case None => BadRequest
+        }
+    }
+
+    def updateProduct(id: Int): Action[AnyContent] = Action { implicit request =>
+        productList.find(_.id == id) match {
+            case Some(existingProduct) =>
+                request.body.asJson.flatMap(Json.fromJson[Product](_).asOpt) match {
+                    case Some(updatedProduct) =>
+                        productList -= existingProduct
+                        val newProduct = updatedProduct.copy(id = id)
+                        productList += newProduct
+                        Ok(Json.toJson(newProduct))
+                    case None => BadRequest(Json.obj("error" -> "Invalid JSON format"))
+                }
+            case None => NotFound
+        }
+    }
+
+    def deleteProduct(id: Int): Action[AnyContent] = Action {
+        productList.find(_.id == id) match {
+            case Some(product) =>
+                productList -= product
+                Ok(Json.obj("message" -> s"Product with ID $id deleted"))
+            case None => NotFound
+        }
+    }
 }
